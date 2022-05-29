4160d47e4fe87cd6b030e12cb8689310d6eb5615
abdo423 <abdelrhmankhaled537@gmail.com>
2022-05-29 03:06:58 +0200

    ya rab

--- OP/MyForm2.h
+++ OP/MyForm2.h
@@ -372,7 +372,7 @@ private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e
 	}
 	else
 	{
-		if (msclr::interop::marshal_as<std::string>(listBox1->Text) == "" || "no trains available") {
+		if (msclr::interop::marshal_as<std::string>(listBox1->Text) == " " || "no trains available") {
 			MessageBox::Show("please select train first");
 			return;
 		}
